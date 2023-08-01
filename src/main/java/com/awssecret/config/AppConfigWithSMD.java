16a219bca21a62abfbf576ba558c34b7842d4815
Sanjay Azhagarsamy <azhagarsa@gmail.com>
2023-08-02 00:24:01 +0530

    Update AppConfigWithSMD.java

--- src/main/java/com/awssecret/config/AppConfigWithSMD.java
+++ src/main/java/com/awssecret/config/AppConfigWithSMD.java
@@ -1,75 +1,75 @@
-package com.awssecret.config;
+//package com.awssecret.config;
 
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.secretsmanager.AWSSecretsManager;
-import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
-import com.google.gson.Gson;
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+//import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+//import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+//import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+//import com.google.gson.Gson;*/
 
-@Configuration
+//@Configuration
 public class AppConfigWithSMD {
 	
 	
-private Gson gson=new Gson();
+//private Gson gson=new Gson();
 	
-	@Bean
-	DataSource dataSource() {
-		AwsSecrets awsSecrets=getSecret();
-		try {
-		System.out.println("jdbc:"+awsSecrets.getEngine()+"://"+awsSecrets.getHost()+":"+awsSecrets.getPort()+"/testDb");
-		System.out.println("userName :"+ awsSecrets.getUsername());
-		System.out.println("password :"+ awsSecrets.getPassword());
-		return DataSourceBuilder.create()
-								.url("jdbc:"+awsSecrets.getEngine()+"://"+awsSecrets.getHost()+":"+awsSecrets.getPort()+"/testDb")
-								.username(awsSecrets.getUsername())
-								.password(awsSecrets.getPassword())
-								.build();
-		}
-		catch (Exception e) {
-			System.out.println("ERROR MESSAGE :"+ e.getMessage());
-			e.printStackTrace();
-			return null;
-		}
-	}
+//	@Bean
+//	DataSource dataSource() {
+//		AwsSecrets awsSecrets=getSecret();
+//		try {
+//		System.out.println("jdbc:"+awsSecrets.getEngine()+"://"+awsSecrets.getHost()+":"+awsSecrets.getPort()+"/testDb");
+//		System.out.println("userName :"+ awsSecrets.getUsername());
+//		System.out.println("password :"+ awsSecrets.getPassword());
+//		return DataSourceBuilder.create()
+//								.url("jdbc:"+awsSecrets.getEngine()+"://"+awsSecrets.getHost()+":"+awsSecrets.getPort()+"/testDb")
+//								.username(awsSecrets.getUsername())
+//								.password(awsSecrets.getPassword())
+//								.build();
+//		}
+//		catch (Exception e) {
+//			System.out.println("ERROR MESSAGE :"+ e.getMessage());
+//			e.printStackTrace();
+//			return null;
+//		}
+//	}
 	
 	
 	
-	 private AwsSecrets getSecret() {
-
-	        String secretName = "db-credential";
-	        String region = "ap-south-1";
-
-
-	        AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
-	                .withRegion(region)
-	                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIASL76ADM5FKK7FYOK", "d+KSGPXG9IPwXsEA/WnQxhpVOJQSeI+UMobgU6st")))
-	                .build();
-
-	        String secret;
-	        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
-	                .withSecretId(secretName);
-	        GetSecretValueResult getSecretValueResult = null;
-
-	        try {
-	            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
-	        } catch (Exception e) {
-	            throw e;
-	        }
-	        if (getSecretValueResult.getSecretString() != null) {
-	            secret = getSecretValueResult.getSecretString();
-	            return gson.fromJson(secret, AwsSecrets.class);
-	        }
+//	 private AwsSecrets getSecret() {
+//
+//	        String secretName = "db-credential";
+//	        String region = "ap-south-1";
 
 
-	        return null;
-	    }
+//	        AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
+//	                .withRegion(region)
+//	                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIASL76ADM5FKK7FYOK", "d+KSGPXG9IPwXsEA/WnQxhpVOJQSeI+UMobgU6st")))
+//	                .build();
+
+//	        String secret;
+//	        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+//	                .withSecretId(secretName);
+//	        GetSecretValueResult getSecretValueResult = null;
+
+//	        try {
+//	            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+//	        } catch (Exception e) {
+//	            throw e;
+//	        }
+//	        if (getSecretValueResult.getSecretString() != null) {
+//	            secret = getSecretValueResult.getSecretString();
+//	            return gson.fromJson(secret, AwsSecrets.class);
+//	        }
+
+
+//	        return null;
+//	    }
 
 }
